737917995608fca6e905abc008f9f90879a16780
dunz <zergcake@gmail.com>
2021-06-09 17:47:14 +0900

    https 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import reactRefresh from '@vitejs/plugin-react-refresh';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [reactRefresh()],
-    base: '/vs/'
+    base: '/vs/',
+    server: {
+        https: true
+    }
 });
